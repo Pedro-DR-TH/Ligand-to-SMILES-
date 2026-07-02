62bec329cdd54ef6033978a0c34716076255853d
Pedro Durao <261281137+Pedro-DR-TH@users.noreply.github.com>
2026-07-02 13:12:06 -0500

    Refactor imports to use relative paths

--- Ligand2SMILES/__init__.py
+++ Ligand2SMILES/__init__.py
@@ -1,3 +1,3 @@
-from Ligand2SMILES import name_to_smiles, search, fuzzy_search, available_names
+from .ligand_lookup import name_to_smiles, search, fuzzy_search, available_names
 
 __all__ = ["name_to_smiles", "search", "fuzzy_search", "available_names"]
